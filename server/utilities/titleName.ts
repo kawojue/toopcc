12f06d09c03897e9a6451e7a9463b2ec9d7f93d4
Raheem Muyiwa <kawojue08@gmail.com>
2023-11-04 10:25:19 +0100

    Update titleName.ts

--- server/utilities/titleName.ts
+++ server/utilities/titleName.ts
@@ -1,12 +1,12 @@
 const titleName = (nameToTitle: string): string => {
     const names = nameToTitle.split(' ')
-    const TitledNames: string[] = []
+    const titledNames: string[] = []
 
     for (const n of names) {
-        TitledNames.push(n.charAt(0).toUpperCase() + n.slice(1).toLowerCase())
+        titledNames.push(n.charAt(0).toUpperCase() + n.slice(1).toLowerCase())
     }
 
-    return TitledNames.join(' ')
+    return titledNames.join(' ')
 }
 
 export default titleName
